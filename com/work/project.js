e9b8b97181eff05a5b40f78d5f56eae32fc79838
zakkturner <30674356+zakkturner@users.noreply.github.com>
2021-02-16 23:04:13 -0500

    finished pop up animation

--- com/work/project.js
+++ com/work/project.js
@@ -1,26 +1,42 @@
 import styles from "./work.module.scss";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-export default function Project() {
+export default function Project({ isActive, setIsActive }) {
   let project = useRef(null);
-
   const onEnter = function () {
-    console.log("enter");
-    gsap.to(project, {
-      duration: 0.8,
-      css: { width: "400px" },
-      ease: "power3.inOut",
-    });
+    if (!isActive) {
+      gsap.to(project, {
+        duration: 0.8,
+        css: { width: "400px" },
+        ease: "power3.inOut",
+      });
+    }
   };
   const onLeave = function () {
-    console.log("enter");
-    gsap.to(project, {
-      duration: 0.8,
-      css: { width: "20px" },
+    if (!isActive) {
+      gsap.to(project, {
+        duration: 0.8,
+        css: { width: "20px" },
+        ease: "power3.inOut",
+      });
+    }
+    console.log(isActive);
+  };
+
+  const clickHandler = function () {
+    setIsActive(true);
+    const tl = gsap.timeline();
+
+    tl.to(project, {
+      duration: 1,
+      css: { position: "fixed" },
       ease: "power3.inOut",
+    }).to(project, {
+      duration: 1,
+      css: { width: "100%", height: "100%", top: 0 },
     });
   };
 
@@ -32,6 +48,7 @@ export default function Project() {
       }}
       onMouseEnter={onEnter}
       onMouseLeave={onLeave}
+      onClick={clickHandler}
     ></div>
   );
 }
